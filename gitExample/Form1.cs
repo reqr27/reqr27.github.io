432d50e880194598b9e893d44f062adc2b5dce4e
reqr27 <reqr27@hotmail.com>
2018-04-05 23:55:37 -0400

    Mensaje

--- gitExample/Form1.cs
+++ gitExample/Form1.cs
@@ -16,5 +16,10 @@ namespace gitExample
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOLA");
+        }
     }
 }
