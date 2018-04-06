86ae3922b9c24daa445165687012644da16b87cb
reqr27 <reqr27@hotmail.com>
2018-04-06 00:11:06 -0400

    Formulario FOrm1

--- gitExample/Form1.cs
+++ gitExample/Form1.cs
@@ -20,6 +20,7 @@ namespace gitExample
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("HOLA");
+            MessageBox.Show("HOLA1");
         }
     }
 }
